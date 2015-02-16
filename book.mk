e0631a6f57b5b5c53823cf3f7d6803d190bce10b
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2015-02-16 18:00:07 -0500

    Importing project

---
+++ book.mk
@@ -0,0 +1,21 @@
+book := mill-utilitarianism
+
+srcdir := src
+
+opf := content.opf
+
+content := toc.xhtml \
+           text/cover.xhtml \
+           text/title-page.xhtml \
+           text/chapter-01.xhtml \
+           text/chapter-02.xhtml \
+           text/chapter-03.xhtml \
+           text/chapter-04.xhtml \
+           text/chapter-05.xhtml \
+           text/colophon.xhtml \
+           style/style.css \
+           img/cover.svg \
+           img/cover.png \
+           img/cc-zero.svg \
+           img/publicdomain.svg \
+           toc.ncx
